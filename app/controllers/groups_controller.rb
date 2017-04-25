b54e77c98150a1455ad37e1ef4ae9ee6ebfe4193
maxgta <maxgta@163.com>
2017-04-25 04:25:55 -0700

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -38,7 +38,10 @@ class GroupsController < ApplicationController
       @group.destroy
       redirect_to groups_path, alert: "Group deleted"
     end
-
+    def show
+        @group = Group.find(params[:id])
+        @posts = @group.posts
+      end
 
   private
    def find_group_and_check_permission
